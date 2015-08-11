b2cc7b12256c1161bc7d0acae2cb2df40ae7a80f
Matthew Skinner <skinnerm@email.arizona.edu>
2015-08-11 09:52:30 -0700

    Write model level validations for league and team

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,2 +1,7 @@
 class League < ActiveRecord::Base
+  validates :name, presence: true
+
+  has_many :teams
+  # has_one :commissioner
+  # has_one :rule_set
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,5 @@
 class Team < ActiveRecord::Base
+  validates :league, :name, presence: true
+
+  belongs_to :league
 end
