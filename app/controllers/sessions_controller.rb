d6552ef57523a1ee8cf53f909f86ce7ff939567d
Matthew Skinner <skinnerm@email.arizona.edu>
2015-08-10 19:33:36 -0700

    write sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,24 @@
 class SessionsController < ApplicationController
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+    if @user
+      login!(@user)
+      redirect_to root_url
+    else
+      @user = User.new({ username: params[:user][:username] })
+      render :new
+    end
+  end
+
+  def destroy
+    current_user && logout!
+  end
 end
