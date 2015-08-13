4eaf00035b5e39abf7d64107c8e38b109c5f615a
Matthew Skinner <skinnerm@email.arizona.edu>
2015-08-13 14:50:54 -0700

    Make add button change to drop button when a player is owned

--- app/assets/javascripts/models/player.js
+++ app/assets/javascripts/models/player.js
@@ -1,3 +1,22 @@
 NoKicker.Models.Player = Backbone.Model.extend({
   urlRoot: "/api/players",
+
+  contract: function() {
+    if (!this._contract) {
+      this._contract = new NoKicker.Models.PlayerContract();
+    }
+    return this._contract;
+  },
+
+  isContracted: function() {
+    return !this.contract().isNew();
+  },
+
+  parse: function(response) {
+    if (response.contract) {
+      this.contract().set(response.contract);
+    }
+    return response;
+  }
+
 })
--- app/views/api/players/index.json.jbuilder
+++ app/views/api/players/index.json.jbuilder
@@ -2,6 +2,6 @@ json.array! @players do |player|
   json.extract! player, :id, :fname, :lname, :position, :team_name
   contract = @league.player_contract(player.id)
   if contract
-    json.contract_id contract, :id
+    json.contract contract, :id
   end
 end
