5340f8df3624743342e4c9d62bf982240154c9ed
Matthew Skinner <skinnerm@email.arizona.edu>
2015-08-18 13:52:16 -0700

    Add functions to hold players to be benched and to be started, function to check that positions match (doesn't work for flex)

--- app/assets/javascripts/views/teams/show.js
+++ app/assets/javascripts/views/teams/show.js
@@ -42,13 +42,73 @@ NoKicker.Views.TeamShow = Backbone.CompositeView.extend({
     this.addSubview('.roster-table', playersView);
   },
 
+  toBench: function () {
+    if (!this._toBench) {
+      this._toBench = [];
+    }
+    return this._toBench;
+  },
+
+  toStart: function () {
+    if (!this._toStart) {
+      this._toStart = [];
+    }
+    return this._toStart;
+  },
+
+  firstToBench: function (rosterSlot) {
+    var first = true;
+    this.toBench().forEach( function (rs) {
+      if (rs.escape("position") === rosterSlot.escape("position")) {
+        first = false;
+      }
+    }.bind(this));
+    return first;
+  },
+
+  subsMatch: function () {
+    var result = true;
+    var benchHash = this.subsPositionHash(this.toBench());
+    var startHash = this.subsPositionHash(this.toStart());
+    
+    Object.keys(benchHash).forEach( function (key) {
+      if (benchHash[key] !== startHash[key]) {
+        result = false;
+      }
+    }.bind(this));
+
+    return result;
+  },
+
+  subsPositionHash: function (subs) {
+    var counts = {};
+    subs.forEach( function (sub) {
+      var pos = sub.escape("position")
+
+      // pos needs to come from player for bench slots.
+      if (pos === "bench") {
+        pos = sub.player().escape("position").toLowerCase();
+      }
+
+      // increment or create count at 1
+      if (!counts[pos]) {
+        counts[pos] =  1;
+      } else {
+        counts[pos] += 1;
+      }
+    }.bind(this));
+
+    return counts;
+  },
+
   updateActions: function (event) {
     var rosterSlotId = $(event.currentTarget).data().rosterSlotId;
-    var rosterSlot = this.model.roster_slots().get(rosterSlotId)
+    var rosterSlot = this.model.roster_slots().get(rosterSlotId);
     var slotPosition = rosterSlot.escape("position");
-    var moveTo = $(event.currentTarget).val()
-    debugger;
-    if (moveTo === "bench") {
+    var moveTo = $(event.currentTarget).val();
+
+    if (moveTo === "bench" && this.firstToBench(rosterSlot)) {
+      this.toBench().push(rosterSlot);
       this.model.roster_slots().each( function (roster_slot) {
         if (
           roster_slot.escape("position") === "bench" &&
@@ -60,8 +120,9 @@ NoKicker.Views.TeamShow = Backbone.CompositeView.extend({
           )
         }
       }.bind(this));
-    } else {
-      
+    } else if (slotPosition === "bench") {
+      this.toStart().push(rosterSlot);
     }
+    debugger;
   }
 });
