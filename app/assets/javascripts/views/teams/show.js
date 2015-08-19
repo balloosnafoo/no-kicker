1ea75c7bb4bacc6351b2ebddf56740756d4c014f
Matthew Skinner <skinnerm@email.arizona.edu>
2015-08-18 17:35:40 -0700

    clean out lingering bits from first try at substitution functionality

--- app/assets/javascripts/views/teams/show.js
+++ app/assets/javascripts/views/teams/show.js
@@ -56,33 +56,6 @@ NoKicker.Views.TeamShow = Backbone.CompositeView.extend({
     this.addSubview('.roster-table', playersView);
   },
 
-  // To be phased out
-  toBench: function () {
-    if (!this._toBench) {
-      this._toBench = [];
-    }
-    return this._toBench;
-  },
-
-  // To be phased out
-  toStart: function () {
-    if (!this._toStart) {
-      this._toStart = [];
-    }
-    return this._toStart;
-  },
-
-  // To be phased out
-  firstToBench: function (rosterSlot) {
-    var first = true;
-    this.toBench().forEach( function (rs) {
-      if (rs.escape("position") === rosterSlot.escape("position")) {
-        first = false;
-      }
-    }.bind(this));
-    return first;
-  },
-
   openPositions: function () {
     var positions = ["qb", "wr", "rb", "te", "flex"]
     counts = this.startingPositionCounts(this.model.roster_slots());
@@ -107,68 +80,10 @@ NoKicker.Views.TeamShow = Backbone.CompositeView.extend({
     }.bind(this));
   },
 
-  // to be phased out
-  subsMatch: function () {
-    var result = true;
-    var benchHash = this.subsPositionHash(this.toBench());
-    var startHash = this.subsPositionHash(this.toStart());
-
-    Object.keys(benchHash).forEach( function (key) {
-      if (benchHash[key] !== startHash[key]) {
-        result = false;
-      }
-    }.bind(this));
-
-    return result;
-  },
-
-  // to be phased out
-  getMatchingSubs: function () {
-    var toBench = this.toBench().pop();
-    for (var i = 0; i < this.toStart().length; i++) {
-      var toStartPos = this.toStart()[i].player().escape("position").toLowerCase();
-      if (toStartPos === toBench.escape("position")) {
-        var toStart = this.toStart()[i]
-        this.toStart().splice(i, 1);
-        break;
-      }
-    }
-    return {toBench: toBench, toStart: toStart};
-  },
-
-  // to be phased out
-  subsPositionHash: function (subs) {
-    var counts = {};
-    subs.forEach( function (sub) {
-      var pos = sub.escape("position")
-
-      // pos needs to come from player for bench slots.
-      if (pos === "bench") {
-        pos = sub.player().escape("position").toLowerCase();
-      }
-
-      // increment or create count at 1
-      if (!counts[pos]) {
-        counts[pos] =  1;
-      } else {
-        counts[pos] += 1;
-      }
-    }.bind(this));
-
-    return counts;
-  },
-
   startingPositionCounts: function (slots) {
     var counts = {};
     slots.forEach( function (slot) {
       var pos = slot.escape("position")
-
-      // // bench slots aren't counted here
-      // if (pos === "bench") {
-      //   return;
-      // }
-
-      // increment or create count at 1
       if (!counts[pos]) {
         counts[pos] =  1;
       } else {
@@ -185,7 +100,7 @@ NoKicker.Views.TeamShow = Backbone.CompositeView.extend({
     var slotPosition = rosterSlot.escape("position");
     var moveTo = $(event.currentTarget).val();
 
-    if (moveTo === "bench" && this.firstToBench(rosterSlot)) {
+    if (moveTo === "bench") {
       rosterSlot.set({
         position: "bench",
         order: this.POSITIONAL_VALUES["bench"]
@@ -204,17 +119,6 @@ NoKicker.Views.TeamShow = Backbone.CompositeView.extend({
         success: this.render.bind(this),
         error: function () { debugger }.bind(this)
       });
-    }
-  },
-
-  substitutePlayers: function (event) {
-    event.preventDefault();
-    if (!this.subsMatch()) {
-      return;
-    }
-    while (this.toBench().length > 0) {
-      subs = this.getMatchingSubs();
-      this.swapSubs(subs);
     }
   },
 });
