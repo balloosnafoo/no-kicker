39b9850138b3e454fb24abe00bb08ad5d061a35a
Matthew Skinner <skinnerm@email.arizona.edu>
2015-08-19 22:00:59 -0700

    Add all of the relevant information to the trade index

--- app/assets/javascripts/models/trade_offer.js
+++ app/assets/javascripts/models/trade_offer.js
@@ -1,33 +1,61 @@
 NoKicker.Models.TradeOffer = Backbone.Model.extend({
   urlRoot: "/api/trade_offers",
 
-  tradeItems: function () {
-    if (!this._tradeItem) {
-      this._tradeItem = new NoKicker.Collections.TradeItems(
+  givenItems: function () {
+    if (!this._givenItems) {
+      this._givenItems = new NoKicker.Collections.TradeItems(
         [], { tradeOffer: this }
       );
     }
 
-    return this._tradeItem;
+    return this._givenItems;
+  },
+
+  receivedItems: function () {
+    if (!this._receivedItems) {
+      this._receivedItems = new NoKicker.Collections.TradeItems(
+        [], { tradeOffer: this }
+      );
+    }
+
+    return this._receivedItems;
   },
 
   trader: function () {
     if (!this._trader) {
-      this._trade = new NoKicker.Models.Team();
+      this._trader = new NoKicker.Models.Team();
     }
 
     return this._trader;
   },
 
+  tradee: function () {
+    if (!this._tradee) {
+      this._tradee = new NoKicker.Models.Team();
+    }
+
+    return this._tradee;
+  },
+
   parse: function (response) {
-    if (response.trade_items) {
-      this.tradeItems().set(response.trade_items, { parse: true });
-      delete response.tradeItems;
+    if (response.received_items) {
+      this.receivedItems().set(response.received_items, { parse: true });
+      delete response.receivedItems;
+    }
+
+    if (response.given_items) {
+      this.givenItems().set(response.given_items, { parse: true });
+      delete response.givenItems;
     }
 
     if (response.trader) {
       this.trader().set(response.trader);
       delete response.trader;
+    }
+
+    if (response.tradee) {
+      this.tradee().set(response.tradee);
+      delete response.tradee;
     }
 
     return response;
--- app/assets/javascripts/views/trade_offers/index_item.js
+++ app/assets/javascripts/views/trade_offers/index_item.js
@@ -5,9 +5,22 @@ NoKicker.Views.TradeOfferIndexItem = Backbone.CompositeView.extend({
 
   className: "trades-index-item",
 
+  initialize: function () {
+    this.receivedItems = this.model.receivedItems();
+    this.givenItems = this.model.givenItems();
+    this.trader = this.model.trader();
+    this.tradee = this.model.tradee();
+
+    this.listenTo(this.model, "sync", this.render);
+  },
+
   render: function () {
     var renderedContent = this.template({
-      tradeOffer: this.model
+      tradeOffer: this.model,
+      receivedItems: this.receivedItems,
+      givenItems: this.givenItems,
+      trader: this.trader,
+      tradee: this.tradee
     });
 
     this.$el.html(renderedContent);
